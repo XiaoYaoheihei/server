3f337020f0869304bcff4e20e04f1fc4a1033089
XiaoYaoheihei <1550949541@qq.com>
2023-06-03 16:53:43 +0800

    test三个基础类

---
+++ test/Channel_test.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <sys/timerfd.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <strings.h>
+#include "../net/EventLoop.h"
+#include "../net/Channel.h"
+
+EventLoop* g_loop;
+void timeout() {
+  std::cout << "time out" << std::endl;
+
+  g_loop->quit();
+}
+
+
+int main() {
+  EventLoop loop;
+  g_loop = &loop;
+
+  int timefd = timerfd_create(CLOCK_MONOTONIC, TFD_NONBLOCK | TFD_CLOEXEC);
+  std::cout << timefd << std::endl;
+  Channel chan(&loop, timefd);
+  chan.setRead(timeout);
+  chan.enableReading();
+
+  struct itimerspec howlong;
+  bzero(&howlong, sizeof howlong);
+  howlong.it_value.tv_sec = 5;
+  timerfd_settime(timefd, 0, &howlong, NULL);    
+
+
+  loop.loop();
+  close(timefd);
+  return 0;
+}
