24330dc3b17e99e0f83711cd153c8904e2acbb4e
XiaoYaoheihei <1550949541@qq.com>
2023-06-06 22:39:25 +0800

    定时器队列类

--- net/TimerQueue.cc
+++ net/TimerQueue.cc
@@ -15,20 +15,78 @@ int creatTimerFd() {
 TimerQueue::TimerQueue(EventLoop* loop) 
   : loop_(loop),
     timerfd(creatTimerFd()),
-    timerfdchannel(loop_, timerfd)
-
-    {
+    timerfdchannel(loop_, timerfd),
+    timers_() {
       timerfdchannel.setRead(std::bind(&TimerQueue::handleRead, this));
       timerfdchannel.enableReading();
+}
+
+TimerQueue::~TimerQueue() {
+  
 }
 
 std::weak_ptr<Timer> TimerQueue::addTimer(TimeCallback cb,
                                           Timer::TimePoint when,
                                           Timer::TimeUnit dur) {
+  std::shared_ptr<Timer> time = std::make_shared<Timer>(std::move(cb), when, dur);
+  addTimerInLoop(time);
   
+  return time;
 }
 
 //处理回调事件
 void TimerQueue::handleRead() {
+  //获取当前时间
+  std::chrono::steady_clock::time_point now = std::chrono::steady_clock::now();
+  //获取已经到时的定时器
+  std::vector<std::shared_ptr<Timer>> expried = getExpired(now);
+  for (auto& i : expried) {
+    if (!i->getDel()) {
+      //没有被标记为删除的话执行回调函数
+      i->run();
+    }
+  }
+  reset(expried, now);
+}
 
+//添加定时器任务到事件循环中去
+void TimerQueue::addTimerInLoop(std::shared_ptr<Timer>& timer) {
+  bool ok = insert(timer);
+  if (ok) {
+    //打印日志显示添加到优先队列成功
+  } else {
+    //添加到优先队列失败
+  }
+}
+
+//获取已经到期的Timer
+std::vector<std::shared_ptr<Timer>> TimerQueue::getExpired(const Timer::TimePoint& now) {
+  std::vector<std::shared_ptr<Timer>> exp;
+  //判断哪些定时器都已经超时了
+  while (!timers_.empty() && timers_.top()->getExpiration() <= now) {
+    //还没有被标记为删除的话
+    if (!timers_.top()->getDel()) {
+      //向后插入到vector中去
+      exp.emplace_back(timers_.top());  
+    }
+    //从优先队列中pop
+    timers_.pop();
+  }
+  return exp;
+}
+
+
+bool TimerQueue::insert(const std::shared_ptr<Timer>& timer) {
+  bool ok = false;
+  if (!timers_.empty()) {
+    timers_.push(timer);
+    ok = true;
+  }
+  return ok;
+}
+
+void TimerQueue::reset(const std::vector<std::shared_ptr<Timer>> &expired, const Timer::TimePoint& now) {
+  for (auto& i : expired) {
+    i->restart(now);
+  }
 }
--- net/TimerQueue.h
+++ net/TimerQueue.h
@@ -3,31 +3,54 @@
 #include "Timer.h"
 #include "Channel.h"
 #include <memory>
+#include <queue>
+
+//自定义的仿函数
+struct TimerCmp
+{
+  bool operator()(std::shared_ptr<Timer>& a, std::shared_ptr<Timer>& b) {
+    if (a->getExpiration() == b->getExpiration() ) {
+      //小顶堆，按照序号的从小到大来进行排列
+      return a->getSequence() > b->getSequence();
+    } else {
+      //小顶堆，按照期望的时间顺序从小到大进行排列
+      return a->getExpiration() > b->getExpiration();
+    }
+  }
+};
+
 
 class EventLoop;
 //定时器队列
 class TimerQueue {
   public:
+    //定时器的优先队列，定时器按照时间顺序从小到大的顺序进行排列
+    using TimerList = std::priority_queue<std::shared_ptr<Timer>, std::vector<std::shared_ptr<Timer>>, TimerCmp>;
     TimerQueue(EventLoop* loop);
     ~TimerQueue();
 
     //添加定时器任务
     std::weak_ptr<Timer> addTimer(TimeCallback cb, Timer::TimePoint when, Timer::TimeUnit dur);
-
+    //取消
+    //cansel()
   private:
-    
+    //添加定时器任务到优先队列当中去
+    void addTimerInLoop(std::shared_ptr<Timer>& timer);
     
     //处理回调事件
     void handleRead();
     //获取相应的Timer
+    //从timers_中移除已经到期的Timer,并且通过vector返回他们
     std::vector<std::shared_ptr<Timer>> getExpired(const Timer::TimePoint& now);
+    //重置Timer
     void reset(const std::vector<std::shared_ptr<Timer>> &expired, const Timer::TimePoint& now);
-    //
+    //插入到优先队列中去
     bool insert(const std::shared_ptr<Timer> &timer);
 
     EventLoop* loop_;
     Channel timerfdchannel;
     int timerfd;
+    TimerList timers_;
 };
 
 #endif
