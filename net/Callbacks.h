ea284e4b96baea0474bdda5b5872d33a62b26ea9
XiaoYaoheihei <1550949541@qq.com>
2023-06-21 21:40:37 +0800

    callback头文件

--- net/Callbacks.h
+++ net/Callbacks.h
@@ -3,9 +3,21 @@
 
 #include <functional>
 
+class EventLoop;
+class TcpConnection;
 
 //定时器回调函数
 typedef std::function<void()> TimeCallback;
 
 typedef std::function<void()> Functor;
+
+typedef std::function<void(EventLoop*)> ThreadInitCallback;
+
+typedef std::function<void (int sockfd, const struct sockaddr_in&)>
+  NewConnectionCallback;
+
+typedef std::shared_ptr<TcpConnection> TcpConnectionptr;
+typedef std::function<const TcpConnectionptr&> ConnectionCallback;
+// typedef std::function<const TcpConnectionptr&, Buffer*, Timestamp> MessageCallback;
+
 #endif
