2f5cb9d6dde24116b03faaa32c0e019586b7e766
XiaoYaoheihei <1550949541@qq.com>
2023-06-22 23:06:10 +0800

    TcpServer类

---
+++ net/TcpServer.cc
@@ -0,0 +1,47 @@
+#include "TcpServer.h"
+#include "Acceptor.h"
+#include "EventLoop.h"
+
+//这个存在是干什么的
+using std::placeholders::_1;
+using std::placeholders::_2;
+using std::placeholders::_3;
+
+TcpServer::TcpServer(EventLoop* loop, const sockaddr_in& addr,
+                     const std::string& name, bool reuseport)
+  : loop_(loop),
+    name_(name),
+    localaddr_(addr),
+    acceptor_(new Acceptor(loop, addr, reuseport)),
+    nextConnId_(0) {
+      acceptor_->setAccpetCallback(std::bind(&TcpServer::newConnection, this, _1, _2));
+}
+
+TcpServer::~TcpServer() {
+
+}
+
+void TcpServer::start() {
+  loop_->runInLoop(std::bind(&Acceptor::listen, acceptor_.get()));
+}
+
+void TcpServer::setConnectionCallback(const ConnectionCallback& cb) {
+  connectioncallback_ = cb;
+}
+
+//为通信fd建立专门的类(可以理解成通用的客户端)进行管理
+//每一个通信fd都对应一个TcpConnection
+void TcpServer::newConnection(int sockfd, const struct sockaddr_in& peeraddr) {
+  char buf[32];
+  snprintf(buf, sizeof(buf), "#%d", nextConnId_);
+  ++nextConnId_;
+  std::string connName = name_+buf;
+
+  //打印相关日志信息
+
+  TcpConnectionptr conn(new TcpConnection(loop_, connName, sockfd, localaddr_, peeraddr));
+  connections_[connName] = conn;
+  conn->setConnectionCallBack(connectioncallback_);
+
+  conn->connectionEstablished();
+}
---
+++ net/TcpServer.h
@@ -0,0 +1,33 @@
+#ifndef NET_TCPSERVER_H
+#define NET_TCPSERVER_H
+#include <map>
+#include "EventLoop.h"
+#include "TcpConnection.h"
+
+class Acceptor;
+class TcpServer {
+  public:
+    TcpServer(EventLoop* loop, const sockaddr_in& addr, const std::string& name, bool reuseport);
+    ~TcpServer();
+
+    void start();
+
+    void setConnectionCallback(const ConnectionCallback& cb);
+    // void setMessageCallback(const MessageCallback& cb);
+
+  private:
+    void newConnection(int sockfd, const struct sockaddr_in& peeraddr);
+
+    typedef std::map<std::string, TcpConnectionptr> ConnectionMap;
+    EventLoop* loop_;
+    const std::string name_;
+    const struct sockaddr_in localaddr_;
+    std::unique_ptr<Acceptor> acceptor_;
+    ConnectionCallback connectioncallback_;
+    // MessageCallback messagecallback_;
+    ConnectionMap connections_;
+    int nextConnId_;
+};
+
+
+#endif
